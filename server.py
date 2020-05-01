312447dfb3302d8af190f7738d140b2e065916d0
Alberto Daniel Badia <bctm.adb.cyan@gmail.com>
2020-05-01 00:23:06 -0300

    add interface traffic and refactor

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-import resourses
+import resources
 import temp_resources
 
 app = Flask(__name__)
@@ -9,13 +9,13 @@ api = Api(app)
 cleaner = temp_resources.TempCleaner()
 cleaner.start()
 
-api.add_resource(resourses.Ping, "/ping/<address>")
-api.add_resource(resourses.ArpPing, "/pingarp")
+api.add_resource(resources.Ping, "/ping/<address>")
+api.add_resource(resources.ArpPing, "/pingarp")
+api.add_resource(resources.InterfaceTraffic, "/interface/traffic")
+api.add_resource(resources.QueueTraffic, "/queue/traffic/<name>")
 
 api.add_resource(temp_resources.TempPing, "/temp/ping")
 api.add_resource(temp_resources.TempInterfaceTraffic, "/temp/interface/traffic")
-
-api.add_resource(resourses.QueueTraffic, "/queue/traffic/<name>")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
